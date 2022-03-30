b0616317a4c1417a5394d380aa456776130f0a8c
kaydabs <kddabanka@uh.edu>
2022-03-29 20:34:31 -0500

    project

---
+++ crud_app/server/database/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect("mongodb+srv://admin:admin123@cluster0.jrtu5.mongodb.net/userdb?retryWrites=true&w=majority")
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
